583f0f6f29d6d5b82f59ab2da750c5aecf8844c2
dan-kuroto <2261268657@qq.com>
2024-03-02 23:26:39 +0800

    fix(config): 修复继承Config时新字段无法初始化的问题

--- gs/config.go
+++ gs/config.go
@@ -2,6 +2,9 @@ package gs
 
 import (
 	"log"
+	"os"
+
+	"gopkg.in/yaml.v3"
 
 	"github.com/dan-kuroto/gin-stronger/config"
 )
@@ -18,15 +21,34 @@ func InitConfig[T config.IConfiguration](config T) error {
 
 	const baseName = "application"
 	// init by application.yml
-	if err := config.InitByYaml(baseName, ""); err != nil {
+	if err := initConfigByYaml(config, baseName, ""); err != nil {
 		return err
 	}
 	// init by application-{env}.yml
 	if config.GetActiveEnv() != "" {
-		if err := config.InitByYaml(baseName, config.GetActiveEnv()); err != nil {
+		if err := initConfigByYaml(config, baseName, config.GetActiveEnv()); err != nil {
 			return err
 		}
 	}
 	log.Println("config load complete")
+	return nil
+}
+
+// If env is empty string, init config by {name}.yml, otherwise {name}-{env}.yml
+func initConfigByYaml[T config.IConfiguration](config T, baseName string, env string) error {
+	var fpath string
+	if env == "" {
+		fpath = baseName + ".yml"
+	} else {
+		fpath = baseName + "-" + env + ".yml"
+	}
+
+	data, err := os.ReadFile(fpath)
+	if err != nil {
+		return err
+	}
+	if err := yaml.Unmarshal(data, &config); err != nil {
+		return err
+	}
 	return nil
 }
