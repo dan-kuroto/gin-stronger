a4434bd6253c45a035ecd444351444050cbf88c9
dan-kuroto <2261268657@qq.com>
2024-05-01 18:40:04 +0800

    feat(gp): 输出时使用gp的ToString

--- go-print/logger.go
+++ go-print/logger.go
@@ -28,6 +28,50 @@ func (l *Logger) UseFormatter(formatter *Formatter) *Logger {
 }
 
 func (l *Logger) UseGpFormatter() *Logger {
-	// TODO: 在Println等情况真正用上gs.Formatter
 	return l.UseFormatter(&DefaultFormatter)
+}
+
+func (l *Logger) convertArgs(v []any) []any {
+	if l.formatter != nil {
+		for i, vv := range v {
+			v[i] = l.formatter.ToString(vv)
+		}
+	}
+	return v
+}
+
+func (l *Logger) Print(v ...any) {
+	l.Logger.Print(l.convertArgs(v)...)
+}
+
+func (l *Logger) Println(v ...any) {
+	l.Logger.Println(l.convertArgs(v)...)
+}
+
+func (l *Logger) Printf(format string, v ...any) {
+	l.Logger.Printf(format, l.convertArgs(v)...)
+}
+
+func (l *Logger) Fatal(v ...any) {
+	l.Logger.Fatal(l.convertArgs(v)...)
+}
+
+func (l *Logger) Fatalln(v ...any) {
+	l.Logger.Fatalln(l.convertArgs(v)...)
+}
+
+func (l *Logger) Fatalf(format string, v ...any) {
+	l.Logger.Fatalf(format, l.convertArgs(v)...)
+}
+
+func (l *Logger) Panic(v ...any) {
+	l.Logger.Panic(l.convertArgs(v)...)
+}
+
+func (l *Logger) Panicln(v ...any) {
+	l.Logger.Panicln(l.convertArgs(v)...)
+}
+
+func (l *Logger) Panicf(format string, v ...any) {
+	l.Logger.Panicf(format, l.convertArgs(v)...)
 }
