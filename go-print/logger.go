2a7bce38b03ad387c3b38b3386727718918f04d1
新檀黎斗 <69581083+dan-kuroto@users.noreply.github.com>
2024-07-28 14:59:49 +0800

    feat(logger): traceid support (#21)

--- go-print/logger.go
+++ go-print/logger.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
 type Logger struct {
@@ -34,58 +36,157 @@ func (l *Logger) ClearFormatter() *Logger {
 	return l.UseFormatter(nil)
 }
 
-func (l *Logger) convertArgs(v []any) []any {
+func (l *Logger) convertArgs(args []any) []any {
 	if l.formatter != nil {
-		for i, vv := range v {
-			v[i] = l.formatter.ToString(vv)
+		for i, arg := range args {
+			args[i] = l.formatter.ToString(arg)
 		}
 	}
-	return v
+	return args
+}
+
+func (l *Logger) getTraceId(ctx *gin.Context) string {
+	if ctx != nil {
+		if traceId := ctx.GetString("X-Trace-Id"); traceId != "" {
+			return "[" + traceId + "]"
+		}
+	}
+	return ""
 }
 
 // region overrides
 
-func (l *Logger) Print(v ...any) {
-	l.Output(2, fmt.Sprint(l.convertArgs(v)...))
+func (l *Logger) Print(args ...any) {
+	args = l.convertArgs(args)
+	l.Output(2, fmt.Sprint(args...))
 }
 
-func (l *Logger) Println(v ...any) {
-	l.Output(2, fmt.Sprintln(l.convertArgs(v)...))
+func (l *Logger) PrintWithCtx(ctx *gin.Context, args ...any) {
+	args = l.convertArgs(args)
+	if traceId := l.getTraceId(ctx); traceId != "" {
+		args = append([]any{traceId + " "}, args...)
+	}
+	l.Output(2, fmt.Sprint(args...))
 }
 
-func (l *Logger) Printf(format string, v ...any) {
-	l.Output(2, fmt.Sprintf(format, l.convertArgs(v)...))
+func (l *Logger) Println(args ...any) {
+	args = l.convertArgs(args)
+	l.Output(2, fmt.Sprintln(args...))
 }
 
-func (l *Logger) Fatal(v ...any) {
-	l.Output(2, fmt.Sprint(l.convertArgs(v)...))
+func (l *Logger) PrintlnWithCtx(ctx *gin.Context, args ...any) {
+	args = l.convertArgs(args)
+	if traceId := l.getTraceId(ctx); traceId != "" {
+		args = append([]any{traceId}, args...)
+	}
+	l.Output(2, fmt.Sprintln(args...))
+}
+
+func (l *Logger) Printf(format string, args ...any) {
+	args = l.convertArgs(args)
+	l.Output(2, fmt.Sprintf(format, args...))
+}
+
+func (l *Logger) PrintfWithCtx(ctx *gin.Context, format string, args ...any) {
+	args = l.convertArgs(args)
+	if traceId := l.getTraceId(ctx); traceId != "" {
+		format = traceId + " " + format
+	}
+	l.Output(2, fmt.Sprintf(format, args...))
+}
+
+func (l *Logger) Fatal(args ...any) {
+	args = l.convertArgs(args)
+	l.Output(2, fmt.Sprint(args...))
 	os.Exit(1)
 }
 
-func (l *Logger) Fatalln(v ...any) {
-	l.Output(2, fmt.Sprintln(l.convertArgs(v)...))
+func (l *Logger) FatalWithCtx(ctx *gin.Context, args ...any) {
+	args = l.convertArgs(args)
+	if traceId := l.getTraceId(ctx); traceId != "" {
+		args = append([]any{traceId + " "}, args...)
+	}
+	l.Output(2, fmt.Sprint(args...))
 	os.Exit(1)
 }
 
-func (l *Logger) Fatalf(format string, v ...any) {
-	l.Output(2, fmt.Sprintf(format, l.convertArgs(v)...))
+func (l *Logger) Fatalln(args ...any) {
+	args = l.convertArgs(args)
+	l.Output(2, fmt.Sprintln(args...))
 	os.Exit(1)
 }
 
-func (l *Logger) Panic(v ...any) {
-	s := fmt.Sprint(l.convertArgs(v)...)
+func (l *Logger) FatallnWithCtx(ctx *gin.Context, args ...any) {
+	args = l.convertArgs(args)
+	if traceId := l.getTraceId(ctx); traceId != "" {
+		args = append([]any{traceId}, args...)
+	}
+	l.Output(2, fmt.Sprintln(args...))
+	os.Exit(1)
+}
+
+func (l *Logger) Fatalf(format string, args ...any) {
+	args = l.convertArgs(args)
+	l.Output(2, fmt.Sprintf(format, args...))
+	os.Exit(1)
+}
+
+func (l *Logger) FatalfWithCtx(ctx *gin.Context, format string, args ...any) {
+	args = l.convertArgs(args)
+	if traceId := l.getTraceId(ctx); traceId != "" {
+		format = traceId + " " + format
+	}
+	l.Output(2, fmt.Sprintf(format, args...))
+	os.Exit(1)
+}
+
+func (l *Logger) Panic(args ...any) {
+	args = l.convertArgs(args)
+	s := fmt.Sprint(args...)
 	l.Output(2, s)
 	panic(s)
 }
 
-func (l *Logger) Panicln(v ...any) {
-	s := fmt.Sprintln(l.convertArgs(v)...)
+func (l *Logger) PanicWithCtx(ctx *gin.Context, args ...any) {
+	args = l.convertArgs(args)
+	if traceId := l.getTraceId(ctx); traceId != "" {
+		args = append([]any{traceId + " "}, args...)
+	}
+	s := fmt.Sprint(args...)
 	l.Output(2, s)
 	panic(s)
 }
 
-func (l *Logger) Panicf(format string, v ...any) {
-	s := fmt.Sprintf(format, l.convertArgs(v)...)
+func (l *Logger) Panicln(args ...any) {
+	args = l.convertArgs(args)
+	s := fmt.Sprintln(args...)
+	l.Output(2, s)
+	panic(s)
+}
+
+func (l *Logger) PaniclnWithCtx(ctx *gin.Context, args ...any) {
+	args = l.convertArgs(args)
+	if traceId := l.getTraceId(ctx); traceId != "" {
+		args = append([]any{traceId}, args...)
+	}
+	s := fmt.Sprintln(args...)
+	l.Output(2, s)
+	panic(s)
+}
+
+func (l *Logger) Panicf(format string, args ...any) {
+	args = l.convertArgs(args)
+	s := fmt.Sprintf(format, args...)
+	l.Output(2, s)
+	panic(s)
+}
+
+func (l *Logger) PanicfWithCtx(ctx *gin.Context, format string, args ...any) {
+	args = l.convertArgs(args)
+	if traceId := l.getTraceId(ctx); traceId != "" {
+		format = traceId + " " + format
+	}
+	s := fmt.Sprintf(format, args...)
 	l.Output(2, s)
 	panic(s)
 }
