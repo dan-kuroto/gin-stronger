6bbab82e80847524635449a795cc7f0a3e04a689
幻夜つかさ <69581083+dan-kuroto@users.noreply.github.com>
2025-05-03 00:08:24 +0800

    feat(logger): 删除原来的日志模块代码，添加zerolog依赖

--- gs/print.go
+++ gs/print.go
@@ -1,11 +1,1 @@
 package gs
-
-import (
-	gp "github.com/dan-kuroto/gin-stronger/go-print"
-)
-
-var formatter = gp.Formatter{BracketColor: true}
-
-func ToString(data any) string {
-	return formatter.ToString(data)
-}
